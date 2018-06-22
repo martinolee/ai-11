44904af496b81eacfdcbfcdda68c6cf124f200c4
Soo-Han Lee <martino_lee@naver.com>
2018-06-22 22:56:14 +0900

    셀을 탭 하면 뷰가 올라옴 tap the cell to bring up the view

--- ai 11/MainViewController.swift
+++ ai 11/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITableViewDataSource {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -19,6 +19,19 @@ class MainViewController: UIViewController, UITableViewDataSource {
 
         self.tableView.tableFooterView = UIView(frame: .zero)
         self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = false
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,5 +47,20 @@ class MainViewController: UIViewController, UITableViewDataSource {
         cell.featureImageView.image = UIImage(named: target.image)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 0:
+            self.performSegue(withIdentifier: "photoObjectDetection", sender: nil)
+        case 1:
+            self.performSegue(withIdentifier: "realTimeObjectDetection", sender: nil)
+        case 2:
+            self.performSegue(withIdentifier: "facialAnalysis", sender: nil)
+        default:
+            return
+        }
     }
 }
