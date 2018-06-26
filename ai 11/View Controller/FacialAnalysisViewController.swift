b7b0c75b2f7769b62d9011952dba6ac898ed79ed
Soo-Han Lee <martino_lee@naver.com>
2018-06-26 12:42:04 +0900

    얼굴 이미지 분석 뷰 기본 설정 Face image analysis view basic set up

---
+++ ai 11/View Controller/FacialAnalysisViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FacialAnalysisViewController.swift
+//  ai 11
+//
+//  Created by 이수한 on 2018. 6. 26..
+//  Copyright © 2018년 이수한. All rights reserved.
+//
+
+import UIKit
+
+class FacialAnalysisViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addPhoto(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let importFromAlbum = UIAlertAction(title: "앨범에서 가져오기", style: .default) { _ in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .savedPhotosAlbum
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
+        }
+        
+        let takePhoto = UIAlertAction(title: "카메라로 찍기", style: .default) { _ in
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .camera
+            picker.cameraCaptureMode = .photo
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        actionSheet.addAction(importFromAlbum)
+        actionSheet.addAction(takePhoto)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+}
