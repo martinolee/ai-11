f1a4322662765c12cdfc72cff8c967332a13a906
Soo-Han Lee <martino_lee@naver.com>
2018-06-26 14:25:46 +0900

    얼굴을 인식해 사각형으로 표시함 Recognizes faces and displays them as rectangles

--- ai 11/View Controller/FacialAnalysisViewController.swift
+++ ai 11/View Controller/FacialAnalysisViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Vision
+import AVFoundation
 
 class FacialAnalysisViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -19,6 +21,16 @@ class FacialAnalysisViewController: UIViewController, UIImagePickerControllerDel
 
     @IBOutlet weak var blurredImageView: UIImageView!
     @IBOutlet weak var selectedImageView: UIImageView!
+    
+    var selectedCiImage: CIImage? {
+        get {
+            if let selectedImage = self.selectedImage {
+                return CIImage(image: selectedImage)
+            } else {
+                return nil
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,6 +70,50 @@ class FacialAnalysisViewController: UIViewController, UIImagePickerControllerDel
         picker.dismiss(animated: true, completion: nil)
         if let uiImage = info[UIImagePickerControllerEditedImage] as? UIImage {
             self.selectedImage = uiImage
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.detectFace()
+            }
+        }
+    }
+    
+    func detectFace() {
+        if let ciImage = self.selectedCiImage {
+            let detectFaceRequest = VNDetectFaceRectanglesRequest(completionHandler: self.handleFaces)
+            let requestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+            
+            do {
+                try requestHandler.perform([detectFaceRequest])
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func handleFaces(request: VNRequest, error: Error?) {
+        if let faces = request.results as? [VNFaceObservation] {
+            DispatchQueue.main.async {
+                self.displayUI(for: faces)
+            }
+        }
+    }
+    
+    func displayUI(for faces: [VNFaceObservation]) {
+        if let faceImage = self.selectedImage {
+            let imageRect = AVMakeRect(aspectRatio: faceImage.size, insideRect: self.selectedImageView.bounds)
+            
+            for face in faces {
+                let w = face.boundingBox.size.width * imageRect.width
+                let h = face.boundingBox.size.height * imageRect.height
+                let x = face.boundingBox.origin.x * imageRect.width
+                let y = imageRect.maxY - (face.boundingBox.origin.y * imageRect.height) - h
+                
+                let layer = CAShapeLayer()
+                layer.frame = CGRect(x: x, y: y, width: w, height: h)
+                layer.borderColor = UIColor.red.cgColor
+                layer.borderWidth = 1
+                self.selectedImageView.layer.addSublayer(layer)
+            }
         }
     }
     
